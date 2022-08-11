098b6b2a9a4c092d7322d1febc1326afa784c667
Eric M <eldin00@gmail.com>
2022-08-11 00:30:48 -0600

    Fix for 'message content' intent.

--- src/dice_commands.rs
+++ src/dice_commands.rs
@@ -1,20 +1,33 @@
 pub mod dice_commands {
-
+ 
     use serenity::{
-        framework::standard::{macros::*, CommandResult},
-        model::prelude::*,
+        framework::standard::{macros::*, CommandResult, Args},
+        model::channel::Message,
         prelude::*,
     };
 
+    #[group]
+    #[commands("roll", "ping")]
+    pub struct General;
+    
     #[command]
-    async fn r(context: &Context, msg: &Message) -> CommandResult {
-        if let Err(why) = msg
-            .channel_id
-            .say(&context.http, msg.content.as_str())
-            .await
-        {
-            println!("Error sending message: {}", why);
+    async fn ping(ctx: &Context, msg: &Message, mut _args: Args) -> CommandResult {
+        if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+            println!("Error sending message: {:?}", why);
         }
+    
         Ok(())
     }
+    
+    #[command]
+    #[aliases("r")]
+    async fn roll(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+        if let Err(why) = msg.channel_id.say(&ctx.http, msg.content.as_str()).await {
+            println!("Error sending message: {:?}", why);
+        }
+    
+        Ok(())
+    }
+        
+
 }
