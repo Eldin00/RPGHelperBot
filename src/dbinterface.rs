cf7f341850ca668f0e4cf61120da47fe2436bbd6
Eldin00 <eldin00@gmail.com>
2022-08-28 23:59:59 -0600

    Update dbinteface to use config directly to get database name instead of requiring db name to be passed in.

--- src/dbinterface.rs
+++ src/dbinterface.rs
@@ -2,12 +2,14 @@ pub mod dbinterface {
 
     use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
     use std::sync::RwLock;
+    use crate::CONF;
 
     lazy_static! {
-        static ref DB_POOL: RwLock<Option<SqlitePool>> = RwLock::new(None);
+       pub static ref DB_POOL: RwLock<Option<SqlitePool>> = RwLock::new(None);
     }
 
-    pub async fn init_db(db_url: &str) {
+    pub async fn init_db() {
+        let db_url = &(*CONF.read().as_deref().unwrap().get_db_url()).to_string();
         if let Ok(db) = DB_POOL.try_write().as_deref_mut() {
             if !Sqlite::database_exists(db_url).await.unwrap_or(false) {
                 Sqlite::create_database(db_url).await.unwrap();
